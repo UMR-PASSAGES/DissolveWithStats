00657067e56b62994779432926f968e07cb3822e
juliepierson <julie.pierson@cnrs.fr>
2016-09-09 16:15:02 +0200

    oops

--- dissolve_stats_dialog.py
+++ dissolve_stats_dialog.py
@@ -294,10 +294,10 @@ class DissolveWithStatsDialog(QtGui.QDialog, Ui_DissolveWithStats):
                     listAttrs = [self.median(y) if y else NULL for y in listAttrs]
                 elif listStats[i] == "Standard deviation":
                     listAttrs = [self.standard_dev(y) if y else NULL for y in listAttrs]
-		        elif listStats[i] == "Concatenation":
-		            listAttrs = [", ".join(y) if y else NULL for y in listAttrs]
-		        elif listStats[i] == "Uniquification":
-		            listAttrs = [", ".join(set(y)) if y else NULL for y in listAttrs]
+                elif listStats[i] == "Concatenation":
+                    listAttrs = [", ".join(y) if y else NULL for y in listAttrs]
+                elif listStats[i] == "Uniquification":
+                    listAttrs = [", ".join(set(y)) if y else NULL for y in listAttrs]
                 # append each field result to listRes
                 listRes.append(listAttrs)
         return listRes
